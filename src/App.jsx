6a5612b85195a72c88f0e87a4a0c17c6bbd8aa7b
ivanvprodanov <ivan.prodanov@maxomedia.ch>
2024-12-11 16:10:21 +0100

    grid wip

--- src/App.jsx
+++ src/App.jsx
@@ -18,17 +18,24 @@ function App() {
             <h2>test column - 12</h2>
           </div>
         </div>
-        <div className="row">
-          <div className="col-8 shadow-sm p-a-400 m-t-300">8.</div>
-          <div className="col-4 shadow-sm p-a-400 m-t-300">4</div>
+        <div className="row m-t-600">
+          <div className="col-8 shadow-sm p-a-400 ">8.</div>
+          <div className="col-4 shadow-sm p-a-400 ">4</div>
         </div>
-        <div className="row">
-          <div className="col-3 shadow-sm p-a-400 m-t-300">3.</div>
-          <div className="col-9 shadow-sm p-a-400 m-t-300">9.</div>
+        <div className="row m-t-600">
+          <div className="col-3 shadow-sm p-a-400 ">3.</div>
+          <div className="col-9 shadow-sm p-a-400 ">9.</div>
         </div>
-        <div className="row">
-          <div className="col-10 shadow-sm p-a-400 m-t-300">2.</div>
-          <div className="col-2 shadow-sm p-a-400 m-t-300">10.</div>
+        <div className="row m-t-600 gx-5">
+          <div className="col-10 shadow-sm p-a-400 ">10.</div>
+          <div className="col-2 shadow-sm p-a-400 ">2.</div>
+        </div>
+        <div className="row m-t-600">
+          <div className="col-2 shadow-sm p-a-400 ">2.</div>
+          <div className="col-2 shadow-sm p-a-400 ">2.</div>
+          <div className="col-4 shadow-sm p-a-400 ">4.</div>
+          <div className="col-2 shadow-sm p-a-400 ">2.</div>
+          <div className="col-2 shadow-sm p-a-400 ">2.</div>
         </div>
       </div>
     </>
